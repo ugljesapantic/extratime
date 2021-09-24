7d2245eabc0dd7f7768348b007ad21d7e104149a
Ugljesa Pantic <ugljesapantic@gmail.com>
2021-09-24 02:26:41 +0200

    using ds

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,8 @@ module.exports = {
   mode: 'jit',
   purge: [
     './pages/**/*.{js,ts,jsx,tsx}', 
-    './src/**/*.{js,ts,jsx,tsx}'
+    './src/**/*.{js,ts,jsx,tsx}',
+    './ds/**/*.{js,ts,jsx,tsx}'
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
