9dd2984138535ea0d47b84557f547381e48e5102
Ugljesa Pantic <ugljesapantic@gmail.com>
2021-09-30 14:16:48 +0200

    removed amplify, one more attempt

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,10 +1,18 @@
+import { DataStore } from "@aws-amplify/datastore";
+import { useCallback } from "react";
 import PrivatePage from "../src/components/PrivatePage";
+// import { Todo } from "../src/models";
 
 export default function Dashboard() {
+
+  // const onClick = useCallback( async () => {
+  //   await DataStore.save(new Todo({name: 'TODO', description: Math.random().toString()}))
+  // }, [])
 
   return (
     <PrivatePage title="Dashboard">
       <div>Dashboard</div>
+      {/* <button onClick={onClick}>Add it</button> */}
     </PrivatePage>
   )
 }
