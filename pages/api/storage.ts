187e029d955fd654fa6b692acf226d80958bd89c
Ugljesa Pantic <ugljesapantic@gmail.com>
2021-10-05 02:32:34 +0200

    added cors

--- pages/api/storage.ts
+++ pages/api/storage.ts
@@ -1,6 +1,29 @@
 import { supabase } from "../../config/supabase"
 
+function initMiddleware(middleware) {
+  return (req, res) =>
+    new Promise((resolve, reject) => {
+      middleware(req, res, (result) => {
+        if (result instanceof Error) {
+          return reject(result)
+        }
+        return resolve(result)
+      })
+    })
+}
+
+// Initialize the cors middleware
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET, POST and OPTIONS
+    methods: ['GET', 'POST', 'OPTIONS'],
+  })
+)
+
 export default async function handler(req, res) {
+  await cors(req, res)
+  
   const {data, error} = await supabase.storage
     .from('test')
     .upload(Math.random().toString(), req.body.data);
