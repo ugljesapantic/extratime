55fa87116f8b560716351b81b43ece54b853fc79
Ugljesa Pantic <ugljesapantic@gmail.com>
2021-11-04 23:46:47 +0100

    widget 1

--- atoms/Widget.tsx
+++ atoms/Widget.tsx
@@ -1,21 +1,25 @@
 import clsx from 'clsx';
+import Link from 'next/link';
 import React, { InputHTMLAttributes } from 'react'
 
 type Props = {
   className?: string;
   title: string;
+  url: string;
 }
 
-const Widget: React.FC<Props> = ({ className, title, children}) => {
+const Widget: React.FC<Props> = ({ className, title, children, url}) => {
 
   
-  return <div className={clsx(
-    'p-4 shadow rounded flex flex-col h-48 w-48 border border-gray-200',
-    className
-  )}>
-    <div className="text-lg mb-2">{title}</div>
-    {children}
-  </div>
+  return <Link href={url}>
+    <div  className={clsx(
+      'p-4 shadow rounded flex flex-col h-48 w-48 border border-gray-200 cursor-pointer',
+      className
+    )}>
+      <div className="text-lg mb-2">{title}</div>
+      {children}
+    </div>
+  </Link>
 };
 
 
--- components/dashboard/BrowseryWidget.tsx
+++ components/dashboard/BrowseryWidget.tsx
@@ -2,23 +2,33 @@ import clsx from 'clsx';
 import React, { InputHTMLAttributes, useCallback, useEffect, useState } from 'react'
 import Widget from '../../atoms/Widget';
 import { supabase } from '../../config/supabase';
+import * as dayjs from 'dayjs'
+import duration from 'dayjs/plugin/duration';
+
+dayjs.extend(duration);
 
 
 
 const BrowserlyWidget: React.FC = () => {
-  const data = useState();
+  const [duration, setDuration] = useState<string>();
   const fetchIt = useCallback(async () => {
     const { data, error } = await supabase
     .from('WebsiteVisit')
     .select()
+
+    // TODO error handling
+    const timeSpent = data?.reduce((prev, curr) => prev + curr.end - curr.start , 0)
+    const d = dayjs.duration(timeSpent, 'milliseconds');
+    setDuration(`${d.format('H')}h:${d.format('m')}m`)
   }, [])
 
   useEffect(() => {
     fetchIt();
   }, [fetchIt])
   
-  return <Widget title="Browserly" >
-    teeest
+  return <Widget title="Browserly" url="/browserly" >
+    {/* TODO add loading */}
+    {duration && <div className="text-2xl">{`Today: ${duration}`}</div>}
   </Widget>
 };
 
