431c8bb48268ee4e99434b39ab4ec2508fa5ddbd
Ugljesa Pantic <ugljesapantic@gmail.com>
2021-11-09 01:07:44 +0100

    df

--- lib/use-pomodoro.ts
+++ lib/use-pomodoro.ts
@@ -3,7 +3,7 @@ import { useEffect, useMemo, useState } from 'react'
 import day from '../lib/day';
 
 export const usePomodoro = () => {
-  const [data, setData] = useState<{left: number; paused: boolean}>() 
+  const [data, setData] = useState<{remaining: number; paused: boolean}>() 
 
   
   useEffect(() => {
@@ -19,11 +19,11 @@ export const usePomodoro = () => {
   }, [])
 
   const timeLeft = useMemo(() => {
-    if (!data?.left) return null;
-    return day.duration(data?.left, 'seconds').format('mm:ss')
-  }, [data])
+    if (!data?.remaining) return null;
+    return day.duration(data?.remaining, 'seconds').format('mm:ss')
+  }, [data?.remaining])
 
-  console.log(timeLeft, data?.paused)
+  console.log(timeLeft, data?.paused, data)
 
   return {timeLeft, hasActive: timeLeft !== null, paused: data?.paused}
 }
